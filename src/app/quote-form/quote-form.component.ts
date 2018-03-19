68e1b369f867fb734c03d51cf81f7ba8df2fc58e
Cynthia Kasambuli <cynthiakasambuli@gmail.com>
2018-03-19 10:55:05 +0300

    add decorators

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -6,15 +6,32 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  author: string = '';
-  onSubmit() {
 
-  }
+  quotes: string[]
+  author: string;
+
   constructor() { }
 
   ngOnInit() {
-    this.author = 'cynthia'
-    console.log(author);
+    this.author = ''
+
+    this.quotes = ['love is life', 'context is king'];
+  }
+  onClick() {
+    this.quotes.unshift('quote');
+
+  }
+  onSubmit(quote) {
+    console.log(quote);
+    return false;
+    this.quotes.push(quote)
+  }
+  deleteQuote(quote) {
+    for (let i = 0; i < this.quotes.length; i++) {
+      if (this.quotes[i] == quote) {
+        this.quotes.splice(i, 1)
+      }
+    }
   }
 
 }
